4538d2ac6c51e54668c6609039430890e1bf6906
Radhika Bacharwar <radhikabacharwar20@gmail.com>
2025-03-02 15:08:04 +0530

    setting convex with clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://saving-firefly-22.clerk.accounts.dev/.well-known/jwks.json",
+        applicationID: "convex",
+      },
+    ]
+  };
