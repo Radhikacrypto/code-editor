d7b9be77c8bfc2d3f4d58d8876dd9b1f6833f723
Radhika Bacharwar <radhikabacharwar20@gmail.com>
2025-05-17 03:22:55 +0530

    making of schema.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -2,7 +2,7 @@
 export default {
   providers: [
     {
-      domain:" https://measured-escargot-5.clerk.accounts.dev",
+      domain:"https://measured-escargot-5.clerk.accounts.dev",
       applicationID: "convex",
     },
   ]
