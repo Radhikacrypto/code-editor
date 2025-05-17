9457f888ad730453cbc0048620b9f4be35e67f52
Radhika Bacharwar <radhikabacharwar20@gmail.com>
2025-05-18 01:32:32 +0530

    making of http.ts

---
+++ convex/http.ts
@@ -0,0 +1,69 @@
+import { httpRouter } from "convex/server";
+import {httpAction} from "./_generated/server"
+import { Webhook } from "svix";
+import { WebhookEvent } from "@clerk/nextjs/server";
+
+const http = httpRouter();
+
+http.route({
+    path:"/clerk-webhook",
+    method:"POST",
+
+    handler: httpAction(async (ctx, request)=>{
+        const webhookSecret = process.env.CLERK_SECRET;
+        if(!webhookSecret) {
+            throw new Error("missing clerk_webhook_secret enviornment variable");
+        }
+
+        const svix_id = request.headers.get("svix-id");
+        const svix_signature = request.headers.get("svix-signature");
+        const svix_timestamp = request.headers.get("svix-timestamp");
+     
+
+        //here we were checking that whether we get or not id , sign and all
+        if(!svix_id || !svix_signature || !svix_timestamp){
+            return new Response(" Errors occured -- no svix headers", {
+                status:400,
+            })
+        }
+
+        const payload= await request.json();
+        const body= JSON.stringify(payload);
+
+        const wh = new Webhook(webhookSecret);
+        let evt : WebhookEvent;
+
+       //here we are verifying that the received id , sign all are same
+        try{
+            evt= wh.verify(body, {
+                "svix-id": svix_id,
+                "svix-signature": svix_signature,
+                "svix-timestamp": svix_timestamp,
+            }) as WebhookEvent;
+        } catch(err){
+            console.error(" Error verifyimg webhook:", err);
+            return new Response("Error occured", {status: 400});
+        }
+
+        const eventType = evt.type;
+        if( eventType==="user.created"){
+            //save user to convex db
+
+            const{ id, email_addresses, first_name, last_name}= evt.data
+
+            const email=email_addresses[0].email_address;
+            const name = `${first_name || ""} ${last_name || ""}`.trim();
+            try {
+                
+                //save user to db
+                
+            } catch (error) {
+                return new Response (" Error cresting user", {status:500});
+            }
+        }
+
+        return new Response(" webhook processed succesfully",{status: 200})
+    })
+})
+
+export default http;
