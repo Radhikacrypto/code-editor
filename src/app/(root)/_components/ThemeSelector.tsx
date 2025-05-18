4f2f048a2d5c55658c6afab4a2a38087d0131730
Radhika Bacharwar <radhikabacharwar20@gmail.com>
2025-05-19 03:04:51 +0530

    making useCodeEditorSelector

--- src/app/(root)/_components/ThemeSelector.tsx
+++ src/app/(root)/_components/ThemeSelector.tsx
@@ -1,6 +1,17 @@
+"use client";
+
+import { useState } from "react";
+import { useRef } from "react";
+import { useCodeEditorStore } from "@/store/useCodeEditorStore";
+import { THEMES } from "../_constants";
 
 
 function ThemeSelector() {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const{theme, setTheme} = useCodeEditorStore();
+  const dropdownRef=useRef<HTMLDivElement>(null);
+  const currentTheme= THEMES.find((t)=>t.id===theme);
   return (
     <div>ThemeSelector</div>
   )
