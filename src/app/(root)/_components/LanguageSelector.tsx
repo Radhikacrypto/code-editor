51e57ee70ed5f585c5e77eb5c8b824db85493f0a
Radhika Bacharwar <radhikabacharwar20@gmail.com>
2025-05-20 09:45:44 +0530

    LanguageSelector.txt

--- src/app/(root)/_components/LanguageSelector.tsx
+++ src/app/(root)/_components/LanguageSelector.tsx
@@ -1,9 +1,74 @@
+import { useCodeEditorStore } from "@/store/useCodeEditorStore";
+import { useState, useRef, useEffect} from "react"
+import { LANGUAGE_CONFIG } from "../_constants";
+import { motion, AnimatePresence } from "framer-motion";
+import Image from "next/image";
+import { ChevronDownIcon, Lock, Sparkles } from "lucide-react";
+import useMounted from "@/hooks/useMounted";
+import { div } from "framer-motion/client";
+
 
 
 function LanguageSelector({hasAccess}:{hasAccess: boolean}) {
+  const[isOpen, setIsOpen]=useState(false);
+  const {language, setLanguage}= useCodeEditorStore();
+  const dropdownRef= useRef<HTMLDivElement>(null);
+    const currentLanguageObj = LANGUAGE_CONFIG[language];//passing laguage as a key 
+  
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  const handleLanguageSelect= (langId:string)=>{}
+
+
   return (
-    <div>LanguageSelector</div>
-  )
+    <div  className="relative" ref={dropdownRef}>
+       <motion.button
+        whileHover={{ scale: 1.02 }}
+        whileTap={{ scale: 0.98 }}
+        onClick={() => setIsOpen(!isOpen)}
+        className={`group relative flex items-center gap-3 px-4 py-2.5 bg-[#1e1e2e]/80 
+      rounded-lg transition-all 
+       duration-200 border border-gray-800/50 hover:border-gray-700
+       ${!hasAccess && language !== "javascript" ? "opacity-50 cursor-not-allowed" : ""}`}
+      >
+        {/* Decoration */}
+        <div
+          className="absolute inset-0 bg-gradient-to-r from-blue-500/10 to-purple-500/5 
+        rounded-lg opacity-0 group-hover:opacity-100 transition-opacity"
+          aria-hidden="true"
+        />
+
+        <div className="size-6 rounded-md bg-gray-800/50 p-0.5 group-hover:scale-110 transition-transform">
+          <Image
+            src={currentLanguageObj.logoPath}
+            alt="programming language logo"
+            width={24}
+            height={24}
+            className="w-full h-full object-contain relative z-10"
+          />
+        </div>
+
+        <span className="text-gray-200 min-w-[80px] text-left group-hover:text-white transition-colors">
+          {currentLanguageObj.label}
+        </span>
+
+        <ChevronDownIcon
+          className={`size-4 text-gray-400 transition-all duration-300 group-hover:text-gray-300
+            ${isOpen ? "rotate-180" : ""}`}
+        />
+      </motion.button>
+
+    </div>
+  );
 }
 
 export default LanguageSelector
