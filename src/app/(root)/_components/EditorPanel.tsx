3e98b68213ab27732c002607533738ff18a7d52a
Radhika Bacharwar <radhikabacharwar20@gmail.com>
2025-05-23 13:19:16 +0530

    editor panel

--- src/app/(root)/_components/EditorPanel.tsx
+++ src/app/(root)/_components/EditorPanel.tsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import { useCodeEditorStore } from '@/store/useCodeEditorStore';
+import React, { useEffect, useState } from 'react'
+import { LANGUAGE_CONFIG } from '../_constants';
 
 function EditorPanel() {
+  const [isShareDialogOpen, setShareDialogOpen] = useState(false);
+  const{ language, theme, fontSize, editor, setFontSize, setEditor} = useCodeEditorStore();
+
+  useEffect(()=>{
+    const savedCode = localStorage.getItem9(`editor-code-${language}`)
+    const newCode= savedCode || LANGUAGE_CONFIG[language].defaultCode
+    if(editor) editor.setValue(newCode)
+  },[language,editor])
+
+  useEffect (()=>{
+    const savedFontSize = localStorage.getItem("editor-font-size");
+    if(savedFontSize) setFontSize(parseInt(savedFontSize));
+
+  },[setFontSize]);
+
+  const handleRefresh  = ()=>{}
+
+  const handleEditorChange = ()=>{}
+
+  const handleFontSizeChange = ()=>{}
   return (
     <div>EditorPanel</div>
   )
