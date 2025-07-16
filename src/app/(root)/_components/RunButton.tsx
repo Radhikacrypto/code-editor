fce946b6729ea06d7cee2d0781880f713622d408
Radhika Bacharwar <radhikabacharwar20@gmail.com>
2025-07-16 20:29:22 +0530

    .

--- src/app/(root)/_components/RunButton.tsx
+++ src/app/(root)/_components/RunButton.tsx
@@ -1,6 +1,13 @@
+"use client"
 
+import { useCodeEditorStore } from "@/store/useCodeEditorStore";
+import{ useUser} from "@clerk/nextjs";
 
 function RunButton() {
+  const {user } = useUser();
+  const{runcode, language , isRunnning , executonResult } = useCodeEditorStore();
+
+  const handleRun = async ()=>{}
   return (
     <div>RunButton</div>
   )
